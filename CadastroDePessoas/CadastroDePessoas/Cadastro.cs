bb1340d55c285f1b34854a38cba6932422400c55
Renan Salvino Martins <36591349880@info.local>
2020-02-14 17:09:40 -0200

    Estudar sobre conexões de banco de dados

---
+++ CadastroDePessoas/CadastroDePessoas/Cadastro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroDePessoas
+{
+    public class Cadastro
+
+
+    {
+        public Cadastro(string nome, string telefone, string email, string cnh)
+        {
+            //Comando Sql -- SqlCommand
+            //parâmetros
+            //conectar com o banco
+            //Executar comando
+            //desconectar
+            //mostrar mensagem de erro ou sucesso
+        }
+    }
+}
