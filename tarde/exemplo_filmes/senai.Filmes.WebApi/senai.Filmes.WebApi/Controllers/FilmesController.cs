bced5939690d55736d893743707a8b12499acbf9
Renan Salvino Martins <36591349880@info.local>
2020-02-19 15:58:14 -0300

    Update

---
+++ tarde/exemplo_filmes/senai.Filmes.WebApi/senai.Filmes.WebApi/Controllers/FilmesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using senai.Filmes.WebApi.Domains;
+using senai.Filmes.WebApi.Interfaces;
+using senai.Filmes.WebApi.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.Filmes.WebApi.Controllers
+{
+    [Produces("application/json")]
+
+    [Route("api/[controller]")]
+
+    [ApiController]
+    public class FilmesController : ControllerBase
+    {
+        private IFilmesRepository _filmeRespository { get; set; }
+
+        public FilmesController()
+        {
+            _filmeRespository = new FilmesRepository();
+        }
+
+
+        [HttpGet]
+        public IEnumerable<FilmeDomain> Get()
+        {
+            return _filmeRespository.Listar();
+        }
+
+
+
+
+     }
+}
---
+++ tarde/exemplo_filmes/senai.Filmes.WebApi/senai.Filmes.WebApi/Interfaces/IFilmesRepository.cs
@@ -0,0 +1,27 @@
+﻿using senai.Filmes.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.Filmes.WebApi.Interfaces
+{
+    interface IFilmesRepository
+    {
+        List<FilmeDomain> Listar();
+
+        void CadastrarFilme(FilmeDomain Filme);
+
+        //void AtualizarIdCorpo(FilmeDomain filme);
+
+        //void AtualizarIdUrl(int id, FilmeDomain filmes);
+
+        //void Deletar(int id);
+
+        //FilmeDomain BuscarPorId(int id);
+
+        
+
+       
+    }
+}
