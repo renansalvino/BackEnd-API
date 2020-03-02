d9a906dd5d3fda23ba7e6506222a519583c5b8cf
renansalvino <renansalvinomartins454@gmail.com>
2020-03-02 14:22:46 -0300

    Atualizando repositório na maquina do Senai

---
+++ CadastroDePessoas/CadastroDePessoas/Conexão.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroDePessoas
+{
+    public class Conexão
+    {
+
+        SqlConnection con = new SqlConnection();
+        //Construtor
+        public Conexão()
+        {  
+            con.ConnectionString = "Data Source=DEV20\\SQLEXPRESS;Initial Catalog=Pessoas;Persist Security Info=True;User ID=sa;Password=sa@132";
+        }
+
+        //Método para se conectar no banco de dados
+        public SqlConnection  conectar()
+
+        {
+            if (con.State == System.Data.ConnectionState.Closed) //se essa  fechada vai executar este código
+                
+            {
+                //conexão estiver aberta ele vai se conectar
+                con.Open();
+
+            }
+            return con;
+        }
+
+        //Método para se desconectar no banco de dados
+
+        public void desconectar()
+        {
+            if (con.State == System.Data.ConnectionState.Open) //se essa conexão estiver fechada vai executar este código
+
+            {
+                con.Close();
+
+            }
+        }
+    }
+}
---
+++ tarde/exemplo_filmes/senai.Filmes.WebApi/senai.Filmes.WebApi/Repositories/FilmesRepository.cs
@@ -0,0 +1,70 @@
+﻿using senai.Filmes.WebApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using senai.Filmes.WebApi.Domains;
+
+namespace senai.Filmes.WebApi.Repositories
+{
+    public class FilmesRepository : IFilmesRepository
+    {
+        private string stringConexao = "Data Source = DEV20\\SQLEXPRESS; initial catalog = Filmes_tarde; user Id = sa; pwd=sa@132";
+
+        public void CadastrarFilme (FilmeDomain filme)
+        {
+            using (SqlConnection con = new SqlConnection(stringConexao))
+            {
+                string queryCadastra = "INSERT INTO Filmes(Titulo,IdGenero) VALUES (@Titulo,@Id)";
+
+                SqlCommand cmd = new SqlCommand(queryCadastra, con);
+
+                cmd.Parameters.AddWithValue("@Titulo", filme.Titulo);
+                cmd.Parameters.AddWithValue("@Id", filme.IdGenero);
+
+                con.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+
+
+        public List<FilmeDomain> Listar()
+        {
+            List<FilmeDomain> filmes = new List<FilmeDomain>  ();
+
+            using (SqlConnection con = new SqlConnection(stringConexao))
+            {
+            string queryTodos = "SELECT IdFilme, Titulo from Filmes";
+
+            con.Open();
+
+                SqlDataReader rdr;
+
+                using (SqlCommand cmd = new SqlCommand(queryTodos, con))
+                {
+                    rdr = cmd.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+                        FilmeDomain filme = new FilmeDomain
+                        {
+                            IdFilme = Convert.ToInt32(rdr[0]),
+
+                            Titulo = rdr["Titulo"].ToString()
+                        };
+
+                        filmes.Add(filme);
+                    }
+                }
+
+            }
+
+            return filmes;
+
+
+        }
+    }
+}
